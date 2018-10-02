77964e4b72fb993d6279d732c77e0334da4867bd
sudhakarmallidi <sudhakar.iphone@gmail.com>
2018-10-03 00:04:44 +0530

    Initial Commit

---
+++ MarketingCloudDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarketingCloudDemo
+//
+//  Created by apple on 03/10/18.
+//  Copyright © 2018 Gautham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
